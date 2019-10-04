90820c38157ad228b919791b9b11d7bd2ce89344
shlroland <shlroland1995@gmail.com>
2019-10-04 16:58:17 +0800

    后台代码提交

---
+++ const.js
@@ -0,0 +1,31 @@
+const resUrl = 'http://localhost:8081/'
+
+const category = [
+    'Biomedicine',
+    'BusinessandManagement',
+    'ComputerScience',
+    'EarthSciences',
+    'Economics',
+    'Engineering',
+    'Education',
+    'Environment',
+    'Geography',
+    'History',
+    'Laws',
+    'LifeSciences',
+    'Literature',
+    'SocialSciences',
+    'MaterialsScience',
+    'Mathematics',
+    'MedicineAndPublicHealth',
+    'Philosophy',
+    'Physics',
+    'PoliticalScienceAndInternationalRelations',
+    'Psychology',
+    'Statistics'
+]
+
+module.exports = {
+    resUrl,
+    category
+}
---
+++ voice.js
@@ -0,0 +1,3 @@
+function creatVoice(req,res) {
+    const text = req.query.text
+}
